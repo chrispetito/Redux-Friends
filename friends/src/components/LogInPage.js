9e97ed352712447656e92f915f28b189ab3d08e9
chrispetito <christopherpetito@gmail.com>
2019-05-15 15:37:35 -0700

    create react app; add reducers; import dependencies; create loginpage file

---
+++ friends/src/components/LogInPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LogInPage = () => {
+    return (
+        <div>
+            <form>
+                <input></input>
+                <input></input>
+                <button>Log In</button>
+            </form>
+        </div>
+    )
+}
+
+export default LogInPage;
---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,12 @@
+// import { } from "../actions";
+
+const initialState = {
+  friends: [],
+};
+export const friendsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+};

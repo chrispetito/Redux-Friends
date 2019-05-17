24251481234365c6833863501052072de50e9b2b
chrispetito <christopherpetito@gmail.com>
2019-05-16 18:22:52 -0700

    require input in new friend forms

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -13,6 +13,7 @@ class FriendForm extends React.Component {
 
 addFriend = event => {
     event.preventDefault();
+    if (!this.state.friend) return;
     this.props.postFriends(this.state.friend);
     this.setState({
         friend: {
@@ -36,9 +37,9 @@ handleChanges = event => {
     return (
       <div className='form-container'>
         <form className='friend-form' onSubmit={this.addFriend}>
-          <input placeholder='Name' name='name' value={this.state.friend.name} onChange={this.handleChanges}/>
-          <input placeholder='Email' name='email' value={this.state.friend.email} onChange={this.handleChanges}/>
-          <input placeholder='Age' name='age' value={this.state.friend.age} onChange={this.handleChanges}/>
+          <input required placeholder='Name' name='name' value={this.state.friend.name} onChange={this.handleChanges}/>
+          <input required placeholder='Email' name='email' value={this.state.friend.email} onChange={this.handleChanges}/>
+          <input required placeholder='Age' name='age' value={this.state.friend.age} onChange={this.handleChanges}/>
           <button>Add Friend</button>
         </form>
       </div>
